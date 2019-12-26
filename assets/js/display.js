850032edf8c98d996ec31db24de0d37177bb06ce
Justin Joyce <justin@joocer.local>
2019-12-26 21:46:45 +0000

    mostly working example

---
+++ assets/js/display.js
@@ -0,0 +1,191 @@
+var height = 150;
+var width = window.innerWidth;
+var margin = {
+    top: 20,
+    right: 25,
+    bottom: 25,
+    left: 50
+};
+width = width - margin.left - margin.right - 25;
+height = height - margin.top - margin.bottom;
+
+var svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function display_initial(ol) {
+    var x_scale = d3.scaleTime().range([0, width]);
+    x_scale.domain(ol.date_range);
+
+    var y_scale = d3.scaleLinear()
+        .domain([0, d3.max(ol.data_index, function(d) {
+            return +d.value;
+        })])
+        .range([height, 0]);
+
+    var x_axis = d3.axisBottom(x_scale);
+    var y_axis = d3.axisLeft(y_scale);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(x_axis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(0,0)")
+        .call(y_axis);
+
+    svg.append("g")
+        .append("path")
+        .datum(ol.data_index)
+        .attr("class", "line")
+        .attr("d", d3.line()
+            .x(function(d) {
+                return x_scale(new Date(d.timestamp));
+            })
+            .y(function(d) {
+                return y_scale(d.value);
+            })
+        )
+
+    var brush = d3.brushX()
+        .extent([
+            [0, 0],
+            [width, height]
+        ])
+        .on("start brush end", brushmoved);
+
+    var gBrush = svg.append("g")
+        .attr("class", "brush")
+        .call(brush);
+
+    var brushResizePath = function(d) {
+        var e = +(d.type == "e"),
+            x = e ? 1 : -1,
+            y = height / 2;
+        return "M" + (.5 * x) + "," + y + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) + "V" + (2 * y - 6) + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "Z" + "M" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) + "M" + (4.5 * x) + "," + (y + 8) + "V" + (2 * y - 8);
+    }
+
+    var handle = gBrush.selectAll(".handle--custom")
+        .data([{
+            type: "w"
+        }, {
+            type: "e"
+        }])
+        .enter().append("path")
+        .attr("class", "handle--custom")
+        .attr("stroke", "#000")
+        .attr("cursor", "ew-resize")
+        .attr("d", brushResizePath);
+
+    function brushmoved() {
+        var s = d3.event.selection;
+        if (s == null) {
+            handle.attr("display", "none");
+        } else {
+            var sx = s.map(x_scale.invert);
+            handle.attr("display", null).attr("transform", function(d, i) {
+                return "translate(" + [s[i], -height / 4] + ")";
+            });
+            document.getElementById("date-range").innerHTML = moment(sx[0]).format(timestampFormat) + " to " + moment(sx[1]).format(timestampFormat);
+            var minCursor = RangeSearch(ol.data, sx[0]);
+            var maxCursor = RangeSearch(ol.data, sx[1]);
+            var matches = maxCursor - minCursor;
+            document.getElementById("status-message").innerHTML = matches + " of " + ol.data.length + " records";
+            DateFilterWrapper(sx);
+        }
+    }
+
+    function DateFilter(date_range) {
+        var minCursor = RangeSearch(ol.data, date_range[0]);
+        var maxCursor = RangeSearch(ol.data, date_range[1]);
+        //console.log(minCursor, maxCursor)
+        var matches = maxCursor - minCursor;
+        var filtered = range(minCursor, maxCursor);
+
+        document.getElementById("status-message").innerHTML = matches + " of " + ol.data.length + " records (" + minCursor + "," + maxCursor + ")";
+
+        if (matches === 0) {
+            document.getElementById("tabular-data").innerHTML = "";
+            return false;
+        }
+        document.getElementById("tabular-data").innerHTML = renderTable(ol.data, filtered);
+    }
+
+    gBrush.call(brush.move, ol.date_range.map(x_scale));
+
+    // this wrapper puts a slight delay on updating the table, this smooths selecting
+    var timer = undefined;
+
+    function DateFilterWrapper(date_range) {
+        if (timer !== null) {
+            window.clearTimeout(timer);
+        }
+        timer = window.setTimeout(function() {
+            DateFilter(date_range);
+            delete timer;
+            timer = undefined;
+        }, 250);
+    }
+}
+
+// TODO: This is sloppy
+// TODO: if its a min, step forward until condition met (duplicates inclusive)
+// TODO: if its a max, step backward until condition met (duplictes inclusive)
+function RangeSearch(arr, x) {
+    x = new Date(x);
+    var mid;
+    let start = 0,
+        end = arr.length - 1;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        var ts = new Date(arr[mid].timestamp);
+        if (ts == x) return mid;
+        else if (ts < x)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    //console.log(new Date(arr[mid].timestamp), '- ', x)
+    return mid;
+}
+
+function range(start, end) {
+    const length = end - start;
+    return Array.from({ length }, (_, i) => start + i);
+}
+
+function htmlEncode(str) {
+    return str.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+        return '&#' + i.charCodeAt(0) + ';'
+    });
+}
+
+// TODO: Sort fields
+// TODO: show/hide fields
+function renderTable(data, indexes) {
+    var row_data = ''
+    row_data += "<tr>"
+    for (var h = 0; h < data.columns.length; h++) {
+        row_data += "<th>" + htmlEncode(data.columns[h]) + "<th>"
+    }
+    row_data += "</tr>"
+    var max_rows = 25;
+    if (indexes.length < max_rows) { max_rows = indexes.length; }
+    for (var i = 0; i < max_rows; i++) {
+        row_data += "<tr>";
+        for (var h = 0; h < data.columns.length; h++) {
+            var cell_value = data[indexes[i]][data.columns[h]];
+            if (is_date(cell_value)) {
+                row_data += "<td>" + moment(data[indexes[i]][data.columns[h]]).format(timestampFormat) + "<td>"
+            } else {
+                row_data += "<td>" + htmlEncode(data[indexes[i]][data.columns[h]]) + "<td>"
+            }
+        }
+        row_data += "</tr>";
+    }
+    return "<table class='table table-striped table-sm'>" + row_data + "</table>"
+}
--- assets/js/orgalog.js
+++ assets/js/orgalog.js
@@ -1,73 +1,78 @@
-// ,
-
-// TODO - select the timestamp format based on the range
-var timestampFormat = "DD MMM YYYY HH:mm:ss"
+const timestampFormat = "DD MMM YYYY HH:mm"
 
 class OrgaLog {
 
     constructor() {
+
+        try {
+            if (moment === undefined) { throw ('Missing Library: moment.js') }
+        } catch (error) {
+            console.error(error);
+            throw ('Missing Library: moment.js')
+        }
+
+        try {
+            if (d3 === undefined) { throw ('Missing Library: d3.js') }
+        } catch (error) {
+            console.error(error);
+            throw ('Missing Library: d3.js')
+        }
+
         this._date_range = undefined;
-        this._time_field = undefined;
+        this._date_field = undefined;
         this._value_range = undefined;
-        this._full_data = [];
+        this._data = [];
         this._data_index = [];
+        this._date_fields = [];
     }
 
     static version() { return "0.0" }
 
-    // from another OrgaLog instance, a file or an array
-    acquire(source, callback) {
-        console.log('TODO: detect the source type')
-        this.acquire_file(source, callback);
-        //        if (arguments.length == 1) { return arguments[0] }
-        //        else if (arguments.length == 2) { return [arguments[0], arguments[1]] }
-    }
-
-    acquire_file(filename, callback) {
-        let self = this;
-        d3.csv(filename).then(function (data) {
-            self._process(data);
-            if (callback !== undefined) {
-                callback();
-            }
-        });
-    }
-
-    _process(data) {
-
-        // persist the full data
-        this._full_data = data;
+    process(data) {
+        // persist the full data, clone it to prevent problems if the original dataset changes
+        this._data = data;
 
         // find the date field
         console.log('TODO: if the field has been specificed but isnt in the data, thrown an error');
-        if (this._time_field !== undefined) {
-            this._time_field = getTimefield(this._full_data);
+        this._date_fields = get_date_fields(this._data);
+        if (this._date_field === undefined) {
+            if (this.date_fields.length > 0) {
+                this._date_field = this._date_fields[0];
+            } else {
+                throw ("No columns contain a known date format")
+            }
+        } else if (!this._date_fields.includes(this._date_field)) {
+            throw ("No columns contain a known date format")
         }
+        console.log('DEBUG: using ', this._date_field, ' as data column');
 
-        // TODO: this is slow - would map() do this faster?
-        // TODO: Change the timestamp format depending on grouping sizes
+        console.log('TODO: this is slow - would map() do this faster?');
         var simplified = []
-        for (var i = 0; i < this._full_data.length; i++) {
+        for (var i = 0; i < this._data.length; i++) {
             simplified.push({
-                timestamp: moment(this._full_data[i][this._time_field]).format(timestampFormat),
+                timestamp: moment(this._data[i][this._date_field]).format(timestampFormat),
                 value: 1
             });
         }
 
         // bin the events by timestamps
         var eventCount = d3.nest()
-            .key(function (d) { return d.timestamp; })
-            .rollup(function (v) { return v.length; })
+            .key(function(d) { return d.timestamp; })
+            .rollup(function(v) { return v.length; })
             .entries(simplified);
 
-        // rename the 'key' field to be 'timestamp'
+        console.log(eventCount);
+
+        console.log('TODO: see if theres anything that can be done to limit the number of new Date() calls needed');
+        // rename the 'key' field to be 'timestamp' (changed by the nest function above)
         for (var i = 0; i < eventCount.length; i++) {
             eventCount[i].timestamp = new Date(eventCount[i]['key']);
             delete eventCount[i].key;
         }
 
-        //
+        // sort the data by the timestamps
         this._data_index = eventCount.sort(function compare(a, b) {
+            //if (a.Display < b.Display) { return -1; } return 1;
             if (new Date(a.timestamp) < new Date(b.timestamp)) {
                 return -1;
             }
@@ -77,70 +82,101 @@ class OrgaLog {
             return 0;
         })
 
-        var min_date = d3.min(this._data_index, function (d) {
+        var min_date = d3.min(this._data_index, function(d) {
             return new Date(d.timestamp);
         });
-        var max_date = d3.max(this._data_index, function (d) {
+        var max_date = d3.max(this._data_index, function(d) {
             return new Date(d.timestamp);
         });
         this._date_range = [min_date, max_date];
 
-        var max_value = d3.max(this._data_index, function (d) {
+        var max_value = d3.max(this._data_index, function(d) {
             return d.value;
         });
-        var min_value = d3.min(this._data_index, function (d) {
+        var min_value = d3.min(this._data_index, function(d) {
             return d.value;
         });
         this._value_range = [min_value, max_value];
     }
 
+    // expose this data so it can be user selected
+    get date_field() { return this._date_field }
+    set date_field(value) {
+        this._date_field = value;
+        this._process(this._data);
+    }
+    get date_fields() { return this._date_fields }
+
+    // range of dates from the data set
+    get date_range() {
+            console.log('date_range:', this._date_range);
+            return this._date_range;
+        }
+        // range of values from the data set 
+    get value_range() { return this._value_range }
+        //
+    get data() { return this._data }
+    get data_index() { return this._data_index }
+
+    view() {
+        var olv = new OrgaLogView(this);
+        return olv;
+    }
+}
+
+class OrgaLogView {
+    constructor() {}
+
+    dedupe() {
+        console.log('dedupe()');
+        var response = Object.create(this);
+
+        // do action on 'response.data'
+        return response;
+    }
+
     range(min, max) {
         console.log('range (', min, ', ', max, ')');
-        return this;
+        var response = Object.create(this);
+        // do action on 'response.data'
+        return response;
     }
 
     filter(filters) {
         console.log('filter (filters)');
-        return this;
+        var response = Object.create(this);
+        // do action on 'response.data'
+        return response;
     }
 
     sort(field, direction) {
         console.log('sort (', field, ',', direction, ')');
-        return this;
+        var response = Object.create(this);
+        // do action on 'response.data'
+        return response;
     }
 
-
-    //
-    get time_field() { return this._time_field }
-    set time_field(value) {
-        this._time_field = value;
-        this._process(this._full_data);
-    }
-
-    // range of dates from the data set
-    get date_range() { return this._date_range }
-    // range of values from the data set 
-    get value_range() { return this._value_range }
-    //
-    get full_data() { return this._full_data }
-    get index() { return this._data_index }
 }
 
 
-function isDate(sDate) {
+function is_date(sDate) {
     if (sDate.toString() == parseInt(sDate).toString()) return false;
     var tryDate = new Date(sDate);
     var m = moment(sDate, ['YYYY-MM-DD', 'YYYYMMDD', 'DD/MM/YYYY', moment.ISO_8601, 'L', 'LL', 'LLL', 'LLLL'], true);
     return (tryDate && tryDate.toString() != "NaN" && tryDate != "Invalid Date" && m.isValid());
 }
 
-function getTimefield(data) {
+// find all of the fields that appear to have dates in them
+function get_date_fields(data) {
+    console.log('TODO: check the first five rows')
+    var date_fields = [];
     var num_columns = data.columns.length;
     for (var h = 0; h < num_columns; h++) {
         var cell_value = data[0][data.columns[h]];
-        if (isDate(cell_value)) {
-            return data.columns[h];
+        if (is_date(cell_value)) {
+            date_fields.push(data.columns[h]);
         }
     }
-    throw "No date field found"
+    console.log("DEBUG: get_date_fields found " + date_fields.length + " date fields", date_fields)
+    return date_fields;
 }
